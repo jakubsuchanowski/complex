191bc3ca659eb35b16a1fb052da1269a17baba84
jakubsuchanowski <kubasuchanowski@gmail.com>
2023-01-28 20:54:24 +0100

    poprawione

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
+      <h2>Multifib</h2>
+      <h2>Jakub Suchanowski</h2>
+      <h2>2.4</h2>
       <Link to="/">Go back home</Link>
     </div>
   );
